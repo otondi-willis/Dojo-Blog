e9ccf24c43aeb7c46d5879e18bb32bcf4e2e1a4e
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-12 09:00:19 +0300

    writing dynamic values

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 function App() {
   const title = "Welcome to the new blog";
   const likes = 50; //React will convert to string then display
-  const person = {name:'yoshi',age:30}; //Objects are not valid as a React child
+  //const person = {name:'yoshi',age:30}; //Objects are not valid as a React child same applies to booleans
 
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
         <p>Liked {likes} times</p>
-        <p>{person}</p>
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <p>{"hello, dunia"}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random()*10}</p>
       </div>
     </div>
   );
