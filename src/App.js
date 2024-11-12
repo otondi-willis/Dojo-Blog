a6a5f42576dac8de1679dbc0c424d35eef1b42c4
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-12 09:23:19 +0300

    Imported and nested Home template

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50; //React will convert to string then display
-  //const person = {name:'yoshi',age:30}; //Objects are not valid as a React child same applies to booleans
- 
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>{title}</h1>
+        <Home/>
       </div>
     </div>
   );
