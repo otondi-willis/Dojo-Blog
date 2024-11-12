35fa3e107f5148d42f0d622cf5527ccd7de8b9c5
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-12 08:53:28 +0300

    Output Multiple values

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 function App() {
   const title = "Welcome to the new blog";
-  
+  const likes = 50;
+
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
+        <p>Liked {likes} times</p>
       </div>
     </div>
   );
