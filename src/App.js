7ceffd2ccf3f7681e16736e4dd63bf00b6cd64e0
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-12 09:17:35 +0300

    importing and nesting Navbar in App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   const title = "Welcome to the new blog";
   const likes = 50; //React will convert to string then display
   //const person = {name:'yoshi',age:30}; //Objects are not valid as a React child same applies to booleans
-  const link = "http://www.google.com";
+ 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{person}</p> */}
-        <p>{10}</p>
-        <p>{"hello, dunia"}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random()*10}</p>
-        <a href={link}>Google Site</a>
-        <p></p>
       </div>
     </div>
   );
