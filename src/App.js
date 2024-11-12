f4546f55bda3443012d8bc8554f6d5728bbe9a93
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-12 08:56:46 +0300

    Objects are not valid as a React child

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 
 function App() {
   const title = "Welcome to the new blog";
-  const likes = 50;
+  const likes = 50; //React will convert to string then display
+  const person = {name:'yoshi',age:30}; //Objects are not valid as a React child
 
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
         <p>Liked {likes} times</p>
+        <p>{person}</p>
       </div>
     </div>
   );
