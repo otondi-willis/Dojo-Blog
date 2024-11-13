ed214dbbb326ae7742f18013ee414baf28db46f3
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-13 14:16:54 +0300

    use of useState()

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,16 @@ import { useState } from 'react';
 const Home = () => {
     //let name = 'willis';//the variable is not reactive
     const [name,setName] = useState('willis');
+    const [age, setAge] = useState(25);
     const handleClick = () => {
         setName('oluigi');
-        
+        setAge(9);
     };
     
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{ name }</p>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
         </div>
      );
