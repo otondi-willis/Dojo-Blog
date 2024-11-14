d9603ed21e2e2981bb1bdf24c4b1e67b542b29f2
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-14 09:08:50 +0300

    Added a blog list

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,8 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         {title:'My new website', body: 'lorem ipsum...', author:'mario', id:1},
         {title:'Welcome party!', body: 'lorem ipsum...', author:'yoshi', id:2},
-        {title:'Web dev top tips', body: 'lorem ipsum...', author:'mario', id:3}
+        {title:'Web dev top tips', body: 'lorem ipsum...', author:'mario', id:3},
+        {title:'Web dev favorites', body: 'lorem ipsum...', author:'mario', id:4}
 
     ]);
     return ( 
