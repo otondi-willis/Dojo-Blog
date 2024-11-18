08b65cb045ee03735ba07527b9696d7e1b578bb6
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-18 08:55:38 +0300

    handleDelete functionality

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,12 @@ const Home = () => {
         {title:'Web dev favorites', body: 'lorem ipsum...', author:'mario', id:4}
 
     ]);
+    const handleDelete = (id)=>{
+
+    }
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
             <BlogList blogs={blogs.filter((blog)=>blog.author==='mario')} title="Mario's blogs!"/>
         </div>
      );
