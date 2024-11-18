16b0745f731fb9dc883602e8e3cee382a674f493
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-18 08:59:41 +0300

    Delete blog functionality

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,8 @@ const Home = () => {
 
     ]);
     const handleDelete = (id)=>{
-
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
     }
     return ( 
         <div className="home">
